ec4e93cd79f8bec9a32cb3bddaa401a70ae5da2a
Davide Saporita <davide.saporita@gmail.com>
2020-04-14 15:14:49 +0200

    Added project files

---
+++ script/main.js
@@ -0,0 +1,17 @@
+/**
+ * DROPDOWN REVOLUT
+ * 
+ * Ricreare la navbar di revolut (vedi grafiche allegate)
+ * Focus è rendere funzionali i menu dropdown all’interno della navigazione principale
+ * Importante l’uso di $(this)
+ * Potete scegliere di usare il click() oppure mouseenter() /mouseleave() per mostrare / nascondere i dropdown menu
+ * Modificare l’icona freccia nel selettore della lingua all’hover (freccia giù / freccia su) usando fontawesome
+ * Nella cartella img allegata trovate 2 grafiche per riprodurre la navbar e il logo
+ *  
+ */
+
+$(document).ready(function(){
+    alert('Hello moto');
+});
+
+
